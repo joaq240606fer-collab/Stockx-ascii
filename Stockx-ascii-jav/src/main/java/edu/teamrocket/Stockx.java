d4d1c317c26021bbfdc6502cfae7a9a90b845c84
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-06 10:33:05 +0200

    feat(Zapatillas): :sparkles: Crear zapatilla
    Crear item zapatilla

---
+++ Stockx-ascii-jav/src/main/java/edu/teamrocket/Stockx.java
@@ -0,0 +1,8 @@
+package edu.teamrocket;
+
+public class Stockx {
+
+    public static void main(String[] args) {
+
+        Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+        System.out.println(Stockx.draw(sneaker));
