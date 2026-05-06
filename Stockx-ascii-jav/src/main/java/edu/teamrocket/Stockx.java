60f6c939efce7c9b106b6e05b4f0dae301f41b98
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-06 10:44:33 +0200

    feat(Ask): :sparkles: Filtrar por ask y tallas
    Filtrar por ask y tallas

--- Stockx-ascii-jav/src/main/java/edu/teamrocket/Stockx.java
+++ Stockx-ascii-jav/src/main/java/edu/teamrocket/Stockx.java
@@ -84,3 +84,8 @@ public class Stockx {
         List<Offer> sizeBid = sizeMaxBid.checkCriteria(sneaker);
         sneaker.setBid(sizeBid.isEmpty()? 0 : sizeBid.get(0).value());
         System.out.println("\n\t\t MAX BID 9.5 US: " + sneaker.getBid());
+
+        Criteria sizeMinAsk = new Min(size, asks);
+        List<Offer> sizeAsk = sizeMinAsk.checkCriteria(sneaker);
+        sneaker.setAsk(sizeAsk.isEmpty()? 0 : sizeAsk.get(0).value());
+        System.out.println("\n\t\t MIN ASK 9.5 US: " + sneaker.getAsk());
