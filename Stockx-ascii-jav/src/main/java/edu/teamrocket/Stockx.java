13212bd4a84c0ae65964f47ea0c8ec1011eec562
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-06 10:47:01 +0200

    fix: corriger sintaxis en método draw()

--- Stockx-ascii-jav/src/main/java/edu/teamrocket/Stockx.java
+++ Stockx-ascii-jav/src/main/java/edu/teamrocket/Stockx.java
@@ -97,18 +97,15 @@ public class Stockx {
     }
 
     public static String draw(Item sneaker) {
-        return
-        "\n\n\t\t" + sneaker.getAsk() + " Buy\t" 
-        + sneaker.getBid() + " Sell \n" +  
-
-        "\t\t" + " _    _" + "\n" +
-        "\t\t" + "(_\\__/(,_" + "\n" +
-        "\t\t" + "| \\ `_////-._" + "\n" +
-        "\t\t" + "J_/___\"=> __/`\\" + "\n" +
-        "\t\t" + "|=====;__/___./" + "\n" +
-        "\t\t" + "\'-\'-\'-\"\"\"\"\"\"\"`" + "\n" +
-        
-        "\t" + sneaker.toString() + "\n" +
-        "\t\tlast sale: " + sneaker.getSale();
+        return "\n\n\t\t" + sneaker.getAsk() + " Buy\t" 
+            + sneaker.getBid() + " Sell \n" +  
+            "\t\t" + " _    _" + "\n" +
+            "\t\t" + "(_\\__/(,_" + "\n" +
+            "\t\t" + "| \\ `_////-._" + "\n" +
+            "\t\t" + "J_/___\"=> __/`\\" + "\n" +
+            "\t\t" + "|=====;__/___./" + "\n" +
+            "\t\t" + "\'-\'-\'-\"\"\"\"\"\"\"`" + "\n" +
+            "\t" + sneaker.toString() + "\n" +
+            "\t\tlast sale: " + sneaker.getSale();
     }
 }
