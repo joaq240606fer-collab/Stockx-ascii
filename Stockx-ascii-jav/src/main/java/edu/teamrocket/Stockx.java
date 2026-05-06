5f7b78f69fc2d09512dd08b99f8e7fa2deed4598
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-06 10:34:06 +0200

    feat(Bids): :sparkles: Crear bids y añadirlas a zapatillas
    Crear bids y añadirlas a zapatillas

--- Stockx-ascii-jav/src/main/java/edu/teamrocket/Stockx.java
+++ Stockx-ascii-jav/src/main/java/edu/teamrocket/Stockx.java
@@ -6,3 +6,11 @@ public class Stockx {
 
         Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
         System.out.println(Stockx.draw(sneaker));
+
+
+        Bid bid = new Bid("13", 550);
+        sneaker.add(bid);
+        sneaker.add(new Bid("6", 200));
+        sneaker.add(new Bid("9.5", 479));
+        sneaker.add(new Bid("13", 338));
+        sneaker.add(new Bid("9.5", 480));
