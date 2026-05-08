6645b176af5b8168fc2b92b103ad027d1e7b2830
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-08 14:28:54 +0200

    feat(sneakkers): :sparkles: Crear clase sneakkers
    Crear clase sneakkers

---
+++ src/main/java/edu/teamrocket/Sneaker.java
@@ -0,0 +1,47 @@
+/*Class Sneaker*/
+
+package edu.teamrocket;
+
+public class Sneaker{
+    private String name;
+    private String style;
+    private int sale;
+    private int ask;
+    private int bid;
+
+
+    public Sneaker(String name, String style) {
+        this.name = name;
+        this.style = style;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getStyle() {
+        return style;
+    }
+    public void setStyle(String style) {
+        this.style = style;
+    }
+    public int getSale() {
+        return sale;
+    }
+    public void setSale(int sale) {
+        this.sale = sale;
+    }
+    public int getAsk() {
+        return ask;
+    }
+    public void setAsk(int ask) {
+        this.ask = ask;
+    }
+    public int getBid() {
+        return bid;
+    }
+    public void setBid(int bid) {
+        this.bid = bid;
+    }
+} 
--- src/main/java/edu/teamrocket/Stockx.java
+++ src/main/java/edu/teamrocket/Stockx.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Stockx {
 
     public static void main(String[] args) {
-        Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
         System.out.println(Stockx.draw(sneaker));
 
         addBids(sneaker);
