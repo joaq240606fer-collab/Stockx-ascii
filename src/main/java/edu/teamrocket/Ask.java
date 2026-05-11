83b833c7aec0f5714d8f37f998fd8f5822b97b8c
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-11 13:40:06 +0200

    feat(Ask): :sparkles: Crear clase ask
    Crear clase ask

---
+++ src/main/java/edu/teamrocket/Ask.java
@@ -0,0 +1,34 @@
+/* ask class */
+package edu.teamrocket;
+
+public class Ask implements Offer {
+    private String size;
+    private int ask;
+    public Ask(String size, int ask) {
+        this.size = size;
+        this.ask = ask;
+    }
+    public String getSize() {
+        return size;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public int getAsk() {
+        return ask;
+    }
+    public void setAsk(int ask) {
+        this.ask = ask;
+    }
+
+    @Override
+    public int compareTo(Offer offer) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
