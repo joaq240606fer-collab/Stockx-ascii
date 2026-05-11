cdcc67fb2a9f5ae83b41ba71ecb5565237a0ca4f
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-11 14:03:52 +0200

    feat(Sales): :sparkles: Crear clases lastsale, maxask
    Crear clases lastsale, maxask

---
+++ src/main/java/edu/teamrocket/LastSale.java
@@ -0,0 +1,17 @@
+package edu.teamrocket;
+import java.util.List;
+import java.util.ArrayList;
+
+public class LastSale implements Criteria {
+    private List<Offer> offers = new ArrayList<>();
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        for (Offer offer : item.getOffers()) {
+            if (offer instanceof Sale) {
+                offers.add(offer);
+            }
+        }
+        return offers;
+    }
+}
