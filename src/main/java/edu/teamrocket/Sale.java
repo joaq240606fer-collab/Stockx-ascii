d399a50429ba6566d48bcee1016ce0d9e6fa3df7
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-11 13:41:36 +0200

    feat(Sales): :sparkles: Crear clase sales
    Crear clase sales

---
+++ src/main/java/edu/teamrocket/Sale.java
@@ -0,0 +1,35 @@
+/* sale class */
+
+package edu.teamrocket;
+
+public class Sale implements Offer {
+    private String size;
+    private int sale;
+    public Sale(String size, int sale) {
+        this.size = size;
+        this.sale = sale;
+    }
+    public String getSize() {
+        return size;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public int getSale() {
+        return sale;
+    }
+    public void setSale(int sale) {
+        this.sale = sale;
+    }
+
+    @Override
+    public int compareTo(Offer offer) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
