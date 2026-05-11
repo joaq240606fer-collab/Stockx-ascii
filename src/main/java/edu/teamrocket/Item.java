c232df254c4b8e6101687476c242f47c6d7aaef6
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-05-11 14:26:44 +0200

    feat(Item): :sparkles: refactorizado de voids
    refactorizado de voids

--- src/main/java/edu/teamrocket/Item.java
+++ src/main/java/edu/teamrocket/Item.java
@@ -6,9 +6,9 @@ public interface Item {
     int getBid();
     int getAsk();
     int getSale();
-    int setBid(int bid);
-    int setAsk(int ask);
-    int setSale(int sale);
+    void setBid(int bid);
+    void setAsk(int ask);
+    void setSale(int sale);
     void add(Offer offer);
     List<Offer> getOffers();
 }
